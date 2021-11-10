bf47a99c9b274f674833f83733b115065a6cb7e9
kzzzzmi <jaemin21685@gmail.com>
2021-11-10 19:46:04 +0900

    learn cookie

--- src/newlecture/study/Calc2.java
+++ src/newlecture/study/Calc2.java
@@ -1,36 +1,78 @@
 package newlecture.study;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
        
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
 		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
-		PrintWriter out = response.getWriter();
+		String v_ = request.getParameter("v");
+		String op = request.getParameter("op");
 		
-		String n1_ = request.getParameter("n1");
-		String n2_ = request.getParameter("n2");
-		String addSub = request.getParameter("add-sub");
+		int v = 0;
 		
-		int n1 = Integer.parseInt(n1_);
-		int n2 = Integer.parseInt(n2_);
+		if(!v_.equals("")) {
+			v = Integer.parseInt(v_);
+		}
 		
-		if(addSub.equals("µ¡¼À")) {
-			out.println("<b>" + n1 + " + " + n2 + " = " + (n1+n2) + "</b>");
+		if(op.equals("=")) {
+			//int x = (Integer)application.getAttribute("value");
+			//int x = (Integer)session.getAttribute("value");
+			Cookie[] cookies = request.getCookies();
+			
+			int x = 0;
+			String operator = "";
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x = Integer.parseInt(c.getValue());
+				} else if(c.getName().equals("operator")) {
+					operator = c.getValue();
+				}
+			}
+			
+			int y = v;
+			//String operator = (String)application.getAttribute("op");
+			//String operator = (String)session.getAttribute("op");
+			int result = 0;
+			
+			if(operator.equals("+")) {
+				result = x + y;
+			} else {
+				result = x - y;
+			}
+			response.getWriter().printf("Return is %d", result);
+			
 		} else {
-			out.println("<b>" + n1 + " - " + n2 + " = " + (n1-n2) + "</b>");
+//			application.setAttribute("value", v);
+//			application.setAttribute("op", op);
+			
+//			session.setAttribute("value", v);
+//			session.setAttribute("op", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("operator", op);
+			valueCookie.setPath("/calc2");
+			valueCookie.setMaxAge(40);
+			opCookie.setPath("/calc2");
+			
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
 		}
 		
 	}
