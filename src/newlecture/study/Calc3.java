7aab495a3d3d848d800e82fce9bbe132d4349c50
kzzzzmi <jaemin21685@gmail.com>
2021-11-15 22:11:12 +0900

    project calc

---
+++ src/newlecture/study/Calc3.java
@@ -0,0 +1,57 @@
+package newlecture.study;
+
+import java.io.IOException;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet {
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Cookie[] cookies = request.getCookies();
+		
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+		
+		String expression = "";
+		
+		if(cookies != null) {
+			for(Cookie c : cookies) {
+				if(c.getName().equals("exp")) {
+					expression = c.getValue();
+					break;
+				}
+					
+			}
+		}		
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("Nashorn");
+			try {
+				expression = engine.eval(expression) + "";
+			} catch (ScriptException e) {
+				e.printStackTrace();
+			}
+		} else {
+			expression += (value == null) ? "" : value;
+			expression += (operator == null) ? "" : operator;
+			expression += (dot == null) ? "" : dot;
+		}
+		
+		Cookie c = new Cookie("exp", expression);
+		response.addCookie(c);
+		response.sendRedirect("/calcPage");
+	}
+
+}
---
+++ src/newlecture/study/CalcPage.java
@@ -0,0 +1,97 @@
+package newlecture.study;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/calcPage")
+public class CalcPage extends HttpServlet {
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		Cookie[] cookies = request.getCookies();
+		String expression = "0";
+		if(cookies != null) {
+			for(Cookie c : cookies) {
+				if(c.getName().equals("exp")) {
+					expression = c.getValue();
+					break;
+				}
+			}
+		}
+		
+		out.write("<!DOCTYPE html>");
+		out.write("<html>");
+		out.write("<head>");
+		out.write("<meta charset=\"UTF-8\">");
+		out.write("<title>Insert title here</title>");
+		out.write("<style>");
+		out.write("input {");
+		out.write("	width: 50px;");
+		out.write("	height: 50px;");
+		out.write("}");
+
+		out.write(".input {");
+		out.write("	text-align: right;");
+		out.write("	background-color: #e9e9e9;");
+		out.write("	font-weight: bold;");
+		out.write("	font-size: 24px;");
+		out.write("	padding: 0px 5px;");
+		out.write("}");
+		out.write("</style>");
+		out.write("</head>");
+		out.write("<body>");
+		out.write("	<form method=\"post\" action=\"calc3\">");
+		out.write("		<table>");
+		out.write("			<tr>");
+		out.printf("				<td colspan=\"4\" class=\"input\">%s</td>", expression);
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"CE\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"C\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"BS\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"/\"></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"7\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"8\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"9\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"*\"></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"4\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"5\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"6\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"-\"></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"1\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"2\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"3\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"+\"></td>");
+		out.write("			</tr>");
+		out.write("			<tr>");
+		out.write("				<td><input type=\"submit\" name=\"value\" value=\"0\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"dot\" value=\".\"></td>");
+		out.write("				<td><input type=\"submit\" name=\"operator\" value=\"=\"></td>");
+		out.write("			</tr>");
+		out.write("		</table>");
+		out.write("	</form>");
+		out.write("</body>");
+		out.write("</html>");
+		
+		
+	}
+
+}
